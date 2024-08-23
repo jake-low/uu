3179d232cfaff71ab327e6e9e367655f3b5a8091
Jake Low <hello@jakelow.com>
2024-08-23 16:12:05 -0700

    Add unit tests for src/utils.js
    This should hopefully make it easier to migrate to the `icu` crate
    (https://github.com/unicode-org/icu4x) once it has support for getting
    character names (https://github.com/unicode-org/icu4x/discussions/5446).

--- src/utils.rs
+++ src/utils.rs
@@ -18,8 +18,7 @@ pub fn repr(c: char) -> String {
 
 /// Returns a string in U+XXXX format representing the code point for the given Unicode character.
 pub fn codepoint(c: char) -> String {
-    // format!("U+{:0>4x}", c as u32)
-    basics::notation::unicode_notation(c).to_string()
+    format!("U+{:0>4X}", c as u32)
 }
 
 /// Returns a string in hexadecimal (with spaces), representing the bytes
@@ -142,5 +141,139 @@ impl CharacterInfo {
         }
 
         return record;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_repr() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "a"),
+            ('2', "2"),
+            ('!', "!"),
+            (' ', " "),
+            ('μ', "μ"),
+            ('チ', "チ"),
+            ('\t', "^I"),   // tab
+            ('\n', "^J"),   // line feed
+            ('\x08', "^H"), // backspace
+            ('\x1b', "^["), // escape
+            ('\0', "^@"),   // null
+        ];
+
+        for (input, expected) in cases {
+            let actual = repr(input);
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn test_codepoint() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "U+0061"),
+            ('2', "U+0032"),
+            ('!', "U+0021"),
+            (' ', "U+0020"),
+            ('μ', "U+03BC"),
+            ('チ', "U+30C1"),
+            ('\t', "U+0009"),   // tab
+            ('\n', "U+000A"),   // line feed
+            ('\x08', "U+0008"), // backspace
+            ('\x1b', "U+001B"), // escape
+            ('\0', "U+0000"),   // null
+            ('�', "U+FFFD"),    // replacement character
+            ('𠃅', "U+200C5"),  // from CJK Extension B
+            // private use areas
+            (char::from_u32(0xE000).unwrap(), "U+E000"), // Private Use Area (BMP)
+            (char::from_u32(0xF0000).unwrap(), "U+F0000"), // Supplementary Private Use Area-A
+            (char::from_u32(0x100000).unwrap(), "U+100000"), // Supplementy Private Use Area-B
+        ];
+
+        for (input, expected) in cases {
+            let actual = codepoint(input);
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn test_char_to_bytes_utf8() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "61"),
+            ('μ', "ce bc"),
+            ('チ', "e3 83 81"),
+            ('�', "ef bf bd"),
+            ('𠃅', "f0 a0 83 85"),
+        ];
+
+        for (input, expected) in cases {
+            let actual = char_to_bytes_utf8(input);
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn test_char_to_bytes_utf16le() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "61 00"),
+            ('μ', "bc 03"),
+            ('チ', "c1 30"),
+            ('�', "fd ff"),
+            ('𠃅', "40 d8 c5 dc"),
+        ];
+
+        for (input, expected) in cases {
+            let actual = char_to_bytes_utf16le(input);
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn test_char_to_bytes_utf16be() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "00 61"),
+            ('μ', "03 bc"),
+            ('チ', "30 c1"),
+            ('�', "ff fd"),        // replacement character
+            ('𠃅', "d8 40 dc c5"), // from CJK Extension B
+        ];
+
+        for (input, expected) in cases {
+            let actual = char_to_bytes_utf16be(input);
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn test_name_or_alias() {
+        let cases: Vec<(char, &str)> = vec![
+            ('a', "LATIN SMALL LETTER A"),
+            ('2', "DIGIT TWO"),
+            ('!', "EXCLAMATION MARK"),
+            (' ', "SPACE"),
+            ('μ', "GREEK SMALL LETTER MU"),
+            ('チ', "KATAKANA LETTER TI"),
+            ('\t', "<CHARACTER TABULATION>"),
+            ('\n', "<LINE FEED>"),
+            ('\x08', "<BACKSPACE>"),
+            ('\x1b', "<ESCAPE>"),
+            ('\0', "<NULL>"),
+            ('\u{0E9D}', "* LAO LETTER FO FON"), // correction of original name
+            ('\u{A015}', "* YI SYLLABLE ITERATION MARK"), // correction of original name
+            // ('ꩮ', "* MYANMAR LETTER KHAMTI LLA"),         // correction of original name (added in Unicode 14, so unsupported by ucd crate)
+            ('�', "REPLACEMENT CHARACTER"),
+            ('𠃅', "CJK UNIFIED IDEOGRAPH-200C5"),
+            // private use areas
+            (char::from_u32(0xE000).unwrap(), ""), // Private Use Area (BMP)
+            (char::from_u32(0xF0000).unwrap(), ""), // Supplementary Private Use Area-A
+            (char::from_u32(0x100000).unwrap(), ""), // Supplementy Private Use Area-B
+        ];
+
+        for (input, expected) in cases {
+            let actual = name_or_alias(input);
+            assert_eq!(expected, actual);
+        }
     }
 }
